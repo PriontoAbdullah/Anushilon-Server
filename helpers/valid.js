cd6609052eca6a8b49e53dc6b6a56b0649bf90f8
PriontoAbdullah <priontosoft@gmail.com>
2021-07-16 12:53:06 +0600

    Server Connected & Models Created

---
+++ helpers/valid.js
@@ -0,0 +1,45 @@
+const { check } = require("express-validator");
+exports.validSign = [
+  check("name", "Name is required")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 32,
+    })
+    .withMessage("name must be between 3 to 32 characters"),
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password", "password is required").notEmpty(),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
+];
+
+exports.validLogin = [
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password", "password is required").notEmpty(),
+  check("password")
+    .isLength({
+      min: 6,
+    })
+    .withMessage("Password must contain at least 6 characters")
+    .matches(/\d/)
+    .withMessage("password must contain a number"),
+];
+
+exports.forgotPasswordValidator = [
+  check("email")
+    .not()
+    .isEmpty()
+    .isEmail()
+    .withMessage("Must be a valid email address"),
+];
+
+exports.resetPasswordValidator = [
+  check("newPassword")
+    .not()
+    .isEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least  6 characters long"),
+];
